a0beb9a3ba00920ab368c66d1178d9f4eb9afc4b
kladiskov <sandesh2k17@gmail.com>
2019-03-24 15:51:33 +0530

    Initial commit for the restaurant finder back end application.

---
+++ src/main/java/com/demo/app/DemoRestaurantFinderApplication.java
@@ -0,0 +1,13 @@
+package com.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestaurantFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestaurantFinderApplication.class, args);
+	}
+
+}
